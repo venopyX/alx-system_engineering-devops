10de58a31822ea99febfcfc2edbcad828c47089a
Gemechis Chala <117309484+venopyX@users.noreply.github.com>
2025-01-10 23:46:28 +0300

    fix: install_a_package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-# This manifest installs Flask version 2.1.0 using pip3.
-package { 'flask':
+# Ensure Python3 and pip3 are installed
+package { 'python3-pip':
+  ensure => present,
+}
+
+# Install Flask version 2.1.0
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
