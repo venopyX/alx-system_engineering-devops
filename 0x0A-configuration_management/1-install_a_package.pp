691ff358da009a7460b42e8c8bd786311c34881f
Gemechis Chala <117309484+venopyX@users.noreply.github.com>
2025-01-11 23:09:53 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,15 @@
-# Ensure Python3 and pip3 are installed
+# Ensure Python3-pip is installed
 package { 'python3-pip':
   ensure => present,
 }
 
-# Install Flask version 2.1.0
+# Install Werkzeug 2.1.0 (compatible with Flask 2.1.0)
+package { 'Werkzeug':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+# Install Flask 2.1.0
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
