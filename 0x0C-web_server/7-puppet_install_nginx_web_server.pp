d1c3fcdb49351b9ef08a0e695edd4f47e57abdeb
venopyX <gladsonchala@gmail.com>
2025-01-14 15:02:06 +0300

    Using puppet for nginx configuration

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,74 +1,24 @@
-# Puppet manifest to configure Nginx with a custom index page, 301 redirect, and a custom 404 error page
+# Setup New Ubuntu server with nginx
 
-# Update system packages
-exec { 'update_system':
-  command => '/usr/bin/apt-get update -qq',
-  path    => ['/bin', '/usr/bin'],
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
 }
 
-# Install Nginx package
 package { 'nginx':
-  ensure  => installed,
-  require => Exec['update_system'],
+        ensure => 'installed',
+        require => Exec['update system']
 }
 
-# Configure the custom index page
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-  require => Package['nginx'],
+file {'/var/www/html/index.html':
+        content => 'Hello World!'
 }
 
-# Configure the custom 404 error page
-file { '/var/www/html/error_404.html':
-  ensure  => file,
-  content => "Ceci n'est pas une page\n",
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-  require => Package['nginx'],
+exec {'redirect_me':
+        command => 'sed -i "24i\        rewrite ^/redirect_me https://scorpidev.me permanent;" /etc/nginx/sites-available/default',
+        provider => 'shell'
 }
 
-# Configure Nginx to include custom rules
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => @(END),
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html;
-
-    server_name _;
-
-    # Default location block
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-
-    # Custom 301 redirect for /redirect_me
-    location /redirect_me {
-        return 301 https://scorpidev.me;
-    }
-
-    # Custom error page for 404
-    error_page 404 /error_404.html;
-    location = /error_404.html {
-        internal;
-    }
-}
-END
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Ensure Nginx service is running
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
+service {'nginx':
+        ensure => running,
+        require => Package['nginx']
 }
